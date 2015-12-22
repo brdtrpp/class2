9931b5951a4de5914c4d66e4ddefb2c55ec2891e
hexes <trapp2357@gmail.com>
2015-12-22 21:01:50 +0000

    changed stripe

--- .demeteorized/bundle/programs/server/app/server/methods/user_methods.js
+++
@@ -1,9 +0,0 @@
-(function(){Meteor.methods({
-  getIP: function(){
-      var ip = this.connection.clientAddress;
-      return ip;
-  }
-});
-}).call(this);
-
-//# sourceMappingURL=user_methods.js.map
--- .demeteorized/bundle/programs/server/app/server/startup.js
+++
@@ -1,6 +0,0 @@
-(function(){Meteor.startup(function() {
-  Stripe = StripeAPI(Meteor.settings.private.stripe.testSecretKey);
-});
-}).call(this);
-
-//# sourceMappingURL=startup.js.map
