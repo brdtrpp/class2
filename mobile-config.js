b7a0f3f4d0312283a8399f8217e445b99921a031
hexes <trapp2357@gmail.com>
2016-01-30 03:07:01 +0000

    updated mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -11,16 +11,16 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-  'iphone': 'images/apple-touch-icon.png',
-  'iphone_2x': 'images/apple-touch-icon.png',
+  'iphone': 'public/images/apple-touch-icon.png',
+  'iphone_2x': 'public/images/apple-touch-icon.png',
   // ... more screen sizes and platforms ...
 });
 
-App.launchScreens({
-  'iphone': 'splash/Default~iphone.png',
-  'iphone_2x': 'splash/Default@2x~iphone.png',
-  // ... more screen sizes and platforms ...
-});
+// App.launchScreens({
+//   'iphone': 'splash/Default~iphone.png',
+//   'iphone_2x': 'splash/Default@2x~iphone.png',
+//   // ... more screen sizes and platforms ...
+// });
 
 // Set PhoneGap/Cordova preferences
 // App.setPreference('BackgroundColor', '0xff0000ff');
