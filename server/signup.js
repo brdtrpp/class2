ddb6807e98e5d02be562054a5a09bac42cf34b15
hexes <trapp2357@gmail.com>
2016-02-02 03:47:24 +0000

    added affiliate tracking

---
+++ server/signup.js
@@ -0,0 +1,14 @@
+
+
+// var mySubmitFunc = function(userId, info){
+//   console.log(affiliateId);
+//   console.log(userId);
+//   console.log(info);
+//   // if (Meteor.user().profile.affiliateId === null) {
+//   //   Meteor.users.update({_id: userId()}, {$set: {affiliateId: affiliateId}});
+//   // }
+// };
+
+// AccountsTemplates.configure({
+//     postSignUpHook: mySubmitFunc
+// });
