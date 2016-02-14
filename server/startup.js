cf8cd0c9e305ed57b383b44aabf7e7590325c3d8
hexes <trapp2357@gmail.com>
2016-02-14 15:26:12 +0000

    updated stripe

--- server/startup.js
+++ server/startup.js
@@ -6,14 +6,16 @@ Meteor.startup(function() {
     'ownerName': 'text'
   });
   
-  SyncedCron.add({
-    name: 'Crunch some important numbers for the marketing department',
-    schedule: function(parser) {
-      // parser is a later.parse object
-      return parser.text('every 2 minutes');
-    },
-    job: function() {
-      console.log('e');
-    }
-  });
+  
+  
+  // SyncedCron.add({
+  //   name: 'Crunch some important numbers for the marketing department',
+  //   schedule: function(parser) {
+  //     // parser is a later.parse object
+  //     return parser.text('every 2 minutes');
+  //   },
+  //   job: function() {
+  //     console.log('e')
+  //   }
+  // });
 });
