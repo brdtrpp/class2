eaf0c96387f87b973d542cf534930615dd0900fe
hexes <trapp2357@gmail.com>
2016-03-07 20:55:00 +0000

    updated beta sending

--- client/hooks/beta_hook.js
+++ client/hooks/beta_hook.js
@@ -5,12 +5,13 @@ AutoForm.hooks({
         if (BetaList.find({email: doc.email}).count() > 0) {
           Bert.alert("You have already sign-up for our beta program.", 'danger');
         } else {
-          Meteor.call('sendEmail',{
+          // SSR.compileTemplate( 'betaList', Assets.getText( 'beta_list.html' ) );
+          Meteor.call('craftEmail',{
+            emailTemplate: 'betaList',
+            asset: 'beta_list.html',
             to: doc.email,
             from: 'support@joinclass.co',
             subject: 'Welcome to Class!',
-            text: 'Thank you for signing up for our beta list.',
-            html: "<h1>Welcome</h1>",
           });
           Bert.alert("Welcome " + doc.email + " to the beta list!");
           return doc;
--- server/mailgun_config.js
+++ server/mailgun_config.js
@@ -8,11 +8,17 @@
   // In your server code: define a method that the client can call
 
 
-
 Meteor.methods({
+  craftEmail: function(mailFields){
+    SSR.compileTemplate( mailFields.emailTemplate, Assets.getText( mailFields.asset ) );
+    mailFields.html = SSR.render( mailFields.emailTemplate );
+    Meteor.call('sendEmail', mailFields);
+  },
+
   sendEmail: function (mailFields) {
+    console.log(mailFields);
       console.log("about to send email...");
-      check([mailFields.to, mailFields.from, mailFields.subject, mailFields.text, mailFields.html], [String]);
+      check([mailFields.to, mailFields.from, mailFields.subject, mailFields.html], [String]);
 
       // Let other method calls from the same client start running,
       // without waiting for the email sending to complete.
@@ -22,7 +28,7 @@ Meteor.methods({
           to: mailFields.to,
           from: mailFields.from,
           subject: mailFields.subject,
-          text: mailFields.text,
+          // text: mailFields.text,
           html: mailFields.html
       });
       console.log("email sent!");
@@ -35,6 +41,6 @@ Meteor.call('sendEmail',{
   to: 'support@joinclass.co',
   from: 'support@joinclass.co',
   subject: 'Deployed',
-  text: 'Deployed',
+  // text: 'Deployed',
   html: "Deployed"
 });
