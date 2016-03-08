c0d5b3374e7e37a7d76d3b16433d89ea3cf7364c
hexes <trapp2357@gmail.com>
2016-03-08 20:01:01 +0000

    added survey

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,4 +1,5 @@
 Meteor.subscribe('calevent');
 Meteor.subscribe('attendee');
 Meteor.subscribe('betaList');
+Meteor.subscribe('allUsers');
 SimpleSchema.debug = true;
--- startup/security.js
+++ startup/security.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
   Security.permit(['insert', 'update']).collections([Attendee, CalEvent, Meteor.users]).ifLoggedIn().apply();
-  Security.permit(['insert', 'update']).collections([Meteor.users]).apply();
-  Security.permit(['insert']).collections([Search, BetaList]).apply();
+  Security.permit(['insert']).collections([Meteor.users]).apply();
+  Security.permit(['insert']).collections([Search, BetaList, SurveyOne]).apply();
 });
 
