06becf48f7e48a6e2e9bb429d2cd23a053a7a03c
hexes <trapp2357@gmail.com>
2016-05-06 18:18:35 +0000

    added permalink

--- server/startup.js
+++ server/startup.js
@@ -15,25 +15,4 @@ Meteor.startup(function() {
     html: "Deployed"
   });
 
-  // var beta = BetaList.find().fetch();
-  // _.forEach(beta, function (user) {
-  //   Meteor.call('craftEmail',{
-  //     emailTemplate: 'betaList',
-  //     asset: 'beta_list.html',
-  //     to: user.email,
-  //     from: 'support@joinclass.co',
-  //     subject: 'Welcome to Class!',
-  //   });
-  // });
-
-  // SyncedCron.add({
-  //   name: 'Crunch some important numbers for the marketing department',
-  //   schedule: function(parser) {
-  //     // parser is a later.parse object
-  //     return parser.text('every 2 minutes');
-  //   },
-  //   job: function() {
-  //     console.log('e')
-  //   }
-  // });
 });
