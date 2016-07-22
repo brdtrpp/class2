7623fe10eb5687fcef70012c22620cddd15cff50
OlesBodlak <oles.bodlak@rst-it.com>
2016-07-22 12:13:04 +0300

    social configurations

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1751686505046416',
+    secret: '2c75544c16b9c3fcc16763b805082a64'
+});
